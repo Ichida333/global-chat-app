0f7aebd707aef806a708f7e689ad543d83cad2d7
ichida333 <obama00300@gmail.com>
2023-09-27 02:35:29 +0900

    login

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,25 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 app.use("/api/user", userRoutes);
+
+
+// --------------------------deployment--------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment--------
 
 
 const PORT = process.env.PORT;
